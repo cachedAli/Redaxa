f56f41b89670af633ddb016cd334a2e7c4deca0c
not-aLi <animeblk2003@gmail.com>
2025-07-12 04:09:04 +0500

    Add Header

---
+++ src/components/ui/Navlink.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function NavLink({
+  href,
+  label,
+}: {
+  href: string;
+  label: string;
+}) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <li
+      className={`transition-colors duration-300 ${
+        isActive ? "text-blue-500" : "text-blue-900 hover:text-teal-500"
+      }`}
+    >
+      <Link href={href}>{label}</Link>
+    </li>
+  );
+}
