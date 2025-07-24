4db4a3c80d434d3643e0fc06e406709f58cc3519
not-aLi <animeblk2003@gmail.com>
2025-07-24 19:26:12 +0500

    Added file removal schedule

--- src/app/api/clean-files/route.ts
+++ src/app/api/clean-files/route.ts
@@ -1,6 +1,11 @@
 import { deleteOldFiles } from "@/lib/server/utils";
 import { NextResponse } from "next/server";
 
+export const config = {
+  schedule: "0 */2 * * *" 
+};
+
+
 export async function GET() {
     try {
         await deleteOldFiles();
